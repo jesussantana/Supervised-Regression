5d39c02a33a95c708d82dc8b3326bcb462e3953b
jesussantana <jesus@esmarketingdigital.com>
2021-05-17 12:52:38 +0200

    Update Settings

---
+++ scripts/exploration.py
@@ -0,0 +1,7 @@
+# Exploration Dataframe
+
+def summary(df):
+    print(df.head()),
+    print(df.info()),
+    print(df.shape),
+    print(df.describe())
--- scripts/libraries.py
+++ scripts/libraries.py
@@ -1,59 +1,60 @@
 # Libraries for Data Science Project
 # ==============================================================================
+#
 
-def main():
+# def main():
 
     
 
 # Data treatment
 # ==============================================================================
-    import numpy as np
-    import pandas as pd
-    from tabulate import tabulate
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
 
 # # Graphics
 # ==============================================================================
-    import matplotlib.pyplot as plt
-    from matplotlib import style
-    import matplotlib.ticker as ticker
-    import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import style
+import matplotlib.ticker as ticker
+import seaborn as sns
 
 # Preprocessing and modeling
 # ==============================================================================
-    from sklearn.model_selection import train_test_split
-    from sklearn.model_selection import GridSearchCV
-    from sklearn.model_selection import RepeatedKFold
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.datasets import make_blobs
-    from sklearn.metrics import euclidean_distances
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_blobs
+from sklearn.metrics import euclidean_distances
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Ridge
 
-    from skopt.space import Real, Integer
-    from skopt.utils import use_named_args
-    from skopt import gp_minimize
-    from skopt.plots import plot_convergence
+from skopt.space import Real, Integer
+from skopt.utils import use_named_args
+from skopt import gp_minimize
+from skopt.plots import plot_convergence
 
 # Various
 # ==============================================================================
-    import multiprocessing
-    import random
-    from itertools import product
-    from fitter import Fitter, get_common_distributions
+import multiprocessing
+import random
+from itertools import product
+from fitter import Fitter, get_common_distributions
 
 # Matplotlib configuration
 # ==============================================================================
-    plt.rcParams['image.cmap'] = "bwr"
-    #plt.rcParams['figure.dpi'] = "100"
-    plt.rcParams['savefig.bbox'] = "tight"
-    style.use('ggplot') or plt.style.use('ggplot')
+plt.rcParams['image.cmap'] = "bwr"
+#plt.rcParams['figure.dpi'] = "100"
+plt.rcParams['savefig.bbox'] = "tight"
+style.use('ggplot') or plt.style.use('ggplot')
 
 # Warnings configuration
 # ==============================================================================
-    import warnings
-    warnings.filterwarnings('ignore')
+import warnings
+warnings.filterwarnings('ignore')
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+ #   main()
